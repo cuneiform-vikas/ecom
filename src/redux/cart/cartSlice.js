51fcab322780928d8f424e681284535f3c503e3c
Vikas <vikas.parmar@thecuneiform.com>
2023-12-07 17:23:58 +0530

    Add essential routes

--- src/redux/cart/cartSlice.js
+++ src/redux/cart/cartSlice.js
@@ -2,6 +2,7 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
   cart: [],
+  items: [],
 };
 
 export const cartSlice = createSlice({
@@ -11,11 +12,31 @@ export const cartSlice = createSlice({
     addToCart: (state, { payload }) => {
       state.cart = [...state.cart, payload];
     },
+    removeFromCart: (state, { payload }) => {
+      state.cart = state.cart.filter((item) => item.id !== payload);
+    },
+    increaseCount: (state, { payload }) => {
+      state.cart = state.cart.map((item) =>
+        item.id === payload ? { ...item, count: item.count + 1 } : item
+      );
+    },
+    decreaseCount: (state, { payload }) => {
+      state.cart = state.cart.map((item) =>
+        item.id === payload ? { ...item, count: item.count - 1 } : item
+      );
+    },
   },
 });
 
 export const selectCart = (state) => state.cart.cart;
+export const selectItems = (state) => state.cart.items;
 
-export const { addToCart } = cartSlice.actions;
+export const {
+  setItems,
+  addToCart,
+  removeFromCart,
+  increaseCount,
+  decreaseCount,
+} = cartSlice.actions;
 
 export default cartSlice.reducer;
