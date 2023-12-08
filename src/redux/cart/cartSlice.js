f0eb0228ff3fe2fbc6ccf54eae6e14fa80ecd5bd
Vikas <vikas.parmar@thecuneiform.com>
2023-12-08 10:35:31 +0530

    Fix bug update cart logic

--- src/redux/cart/cartSlice.js
+++ src/redux/cart/cartSlice.js
@@ -2,7 +2,6 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
   cart: [],
-  items: [],
 };
 
 export const cartSlice = createSlice({
@@ -10,7 +9,15 @@ export const cartSlice = createSlice({
   initialState,
   reducers: {
     addToCart: (state, { payload }) => {
-      state.cart = [...state.cart, payload];
+      const existingItem = state.cart.find((item) => item.id === payload.id);
+
+      if (existingItem) {
+        state.cart = state.cart.map((item) =>
+          item.id === payload.id ? { ...item, count: item.count + 1 } : item
+        );
+      } else {
+        state.cart = [...state.cart, { ...payload, count: 1 }];
+      }
     },
     removeFromCart: (state, { payload }) => {
       state.cart = state.cart.filter((item) => item.id !== payload);
@@ -22,21 +29,17 @@ export const cartSlice = createSlice({
     },
     decreaseCount: (state, { payload }) => {
       state.cart = state.cart.map((item) =>
-        item.id === payload ? { ...item, count: item.count - 1 } : item
+        item.id === payload
+          ? { ...item, count: Math.max(item.count - 1, 1) }
+          : item
       );
     },
   },
 });
 
 export const selectCart = (state) => state.cart.cart;
-export const selectItems = (state) => state.cart.items;
 
-export const {
-  setItems,
-  addToCart,
-  removeFromCart,
-  increaseCount,
-  decreaseCount,
-} = cartSlice.actions;
+export const { addToCart, removeFromCart, increaseCount, decreaseCount } =
+  cartSlice.actions;
 
 export default cartSlice.reducer;
